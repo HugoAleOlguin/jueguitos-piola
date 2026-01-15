1e2d31a834fc251de6ceb9412ae8788eed92ac95
HugoAleOlguin <124766373+HugoAleOlguin@users.noreply.github.com>
2026-01-15 17:28:41 -0300

    ~ prueba

--- beta/assets/js/games.js
+++ beta/assets/js/games.js
@@ -448,7 +448,7 @@ const gamesData = [
         "id": "prueba",
         "title": "prueba",
         "description": "solo es una prueba ok?",
-        "image": "https://c10.patreonusercontent.com/4/patreon-media/p/campaign/10801708/7398ceb271f14d148059375bb6253af9/eyJ3Ijo2MjB9/1.png?token-hash=lRVrL5wx14U-M-tqC6r1JPpSv9hGEol1UhB4aF5ULdI%3D&token-time=1769472000",
+        "image": "https://c.tenor.com/VBDo4XlIMCUAAAAC/tenor.gif",
         "tags": [
             "Party",
             "Puzzle"
