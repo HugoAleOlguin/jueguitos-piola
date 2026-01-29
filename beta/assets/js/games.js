6ac22bc10f289b666b810200dc8f4c0f4e75a626
HugoAleOlguin <124766373+HugoAleOlguin@users.noreply.github.com>
2026-01-29 02:28:53 -0300

    ~ otra prueba (v2026.01.29)

--- beta/assets/js/games.js
+++ beta/assets/js/games.js
@@ -447,12 +447,6 @@ const gamesData = [
       "Party",
       "Puzzle"
     ],
-    "buttons": [
-      {
-        "label": "Descargar",
-        "url": "https://www.mediafire.com/file/wdeb2ste29s88kf/Furry+Love_b13554090_ElEnemigos.rar/file",
-        "style": "primary"
-      }
-    ]
+    "buttons": []
   }
 ];
